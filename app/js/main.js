ce5c399ce1e3f7e8599e5fe796ce070d5682d5bc
DAGERON <den-kir1@yandex.ru>
2021-12-21 01:59:31 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,14 @@ $(function() {
         const header = document.querySelector('.header__top-inner');
         header.classList.toggle("sticky", window.scrollY > 0)
     })
+
+    // Slider
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    });
 });
 
