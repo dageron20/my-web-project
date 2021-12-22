91525c4c0090db4c384fd9212f291546c4e0d201
DAGERON <den-kir1@yandex.ru>
2021-12-22 14:57:43 +0300

    finish adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -35,5 +35,51 @@ $(function() {
             $('#loadMore').fadeOut('slow')
         }
     });
+
+    //menu
+
+    $(".hamburger").on('click', function () {
+        $(this).toggleClass("on");
+      });
+
+      $("ul.main-menu li").on('click', function (e) {
+        e.preventDefault();
+    
+        if ($(this).siblings("li").find("ul.sidebar__submenu:visible").length) {
+          $("ul.sidebar__submenu").slideUp("normal");
+        }
+        $(this).find("ul.sidebar__submenu").slideToggle("normal");
+      });
+
+      var t1 = new TimelineMax({ paused: true });
+
+  t1.to(".menu", 0.1, {
+    autoAlpha: 1,
+  });
+
+  t1.staggerFrom(".main-menu li a:not(.sidebar__submenu li a)", 0.5,
+    {
+      opacity: 0,
+      y: 20,
+      ease: Power3.easeInOut,
+    },
+    0.1
+  );
+
+  t1.from(".sidebar__submenu", 0.3, {
+    autoAlpha: 0,
+  });
+
+  t1.reverse();
+
+  $(document).on("click", ".menu-btn", function () {
+    t1.reversed(!t1.reversed());
+  });
+
+  $(document).on("click", ".close-menu", function () {
+    t1.reversed(!t1.reversed());
+  });
+
+
 });
 
